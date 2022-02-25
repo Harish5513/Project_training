7c30cfefce40848267bfda62c1e5bdbeeb05cb10
Harish5513 <96466381+Harish5513@users.noreply.github.com>
2022-02-25 17:32:38 +0530

    Delete copy_file-task1_1.py

--- copy_file-task1_1.py
+++
@@ -1,7 +0,0 @@
-def task_1():
-    with open('/home/admin-pc/text.txt', 'r') as inp:
-        y = inp.read().upper()
-    with open('/home/admin-pc/copied/text.txt', 'a') as out:
-        out.write(y)
-
-#task_1()
